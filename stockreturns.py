01b93de70b87aac7e8b6cd30a4f745b25fe48506
Eric Brody-Moore <eric@joindrift.com>
2017-09-12 13:18:21 -0700

    adding new file

---
+++ stockreturns.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import random
+import csv
+
+Portfolio = pd.read_csv('stocks.csv')
+todayReturn = round(random.uniform(-.05, .05), 3)
+print('Market return: ' + str(todayReturn))
+print('Return over market:')
+for k in Portfolio:
+	StockReturn = round(random.uniform(-.07, .07), 3)
+	ReturnOverMarket = round(StockReturn - todayReturn, 3)
+	if ReturnOverMarket >= .02:
+		print(k + ': ' + str(ReturnOverMarket) + ' --> INCREASE POSITION')
+	elif ReturnOverMarket <= -.02:
+		print(k + ': ' + str(ReturnOverMarket) + ' --> SELL POSITION')
+	else:
+		print(k + ': ' + str(ReturnOverMarket) + ' --> HOLD POSITION')
